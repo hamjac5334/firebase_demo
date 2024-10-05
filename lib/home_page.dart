724ee2d669a7c07f6f92737aa46951d2c428faf2
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2024-10-05 15:14:07 -0500

    Mistake

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -50,7 +50,17 @@ class HomePage extends StatelessWidget {
             builder: (context, appState, _) => Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              switch (appState.attendees) {
+                1 => const Paragraph('1 person going'),
+                >= 2 => Paragraph('${appState.attendees} people going'),
+                _ => const Paragraph('No one going'),
+              },
               if (appState.loggedIn) ...[
+                YesNoSelection(
+                  state: appState.attending,
+                  onSelection: (attending) => appState.attending = attending,
+                ),
+
               const Header('Discussion'),
               GuestBook(
                 addMessage: (message) =>
