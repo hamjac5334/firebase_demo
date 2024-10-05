6967a0cdeaae0816accec37938f93b85ef769054
hamjac5334 <50788084+hamjac5334@users.noreply.github.com>
2024-10-05 15:13:40 -0500

    Don't know why these did not commit

--- lib/app_state.dart
+++ lib/app_state.dart
@@ -10,6 +10,8 @@ import 'package:flutter/material.dart';
 import '../firebase_options.dart';
 import '../guest_book_message.dart'; 
 
+enum Attending { yes, no, unknown }
+
 StreamSubscription<QuerySnapshot>? _guestBookSubscription;
   List<GuestBookMessage> _guestBookMessages = [];
   List<GuestBookMessage> get guestBookMessages => _guestBookMessages;
@@ -17,7 +19,24 @@ StreamSubscription<QuerySnapshot>? _guestBookSubscription;
 class ApplicationState extends ChangeNotifier {
   ApplicationState() {
     init();
+    int _attendees = 0;
+    int get attendees => _attendees;
+
+    Attending _attending = Attending.unknown;
+    StreamSubscription<DocumentSnapshot>? _attendingSubscription;
+    Attending get attending => _attending;
+    set attending(Attending attending) {
+      final userDoc = FirebaseFirestore.instance
+        .collection('attendees')
+        .doc(FirebaseAuth.instance.currentUser!.uid);
+      if (attending == Attending.yes) {
+        userDoc.set(<String, dynamic>{'attending': true});
+      } else {
+        userDoc.set(<String, dynamic>{'attending': false});
   }
+}
+  }
+}
 
   bool _loggedIn = false;
   bool get loggedIn => _loggedIn;
@@ -29,6 +48,15 @@ class ApplicationState extends ChangeNotifier {
     FirebaseUIAuth.configureProviders([
       EmailAuthProvider(),
     ]);
+
+    FirebaseFirestore.instance
+        .collection('attendees')
+        .where('attending', isEqualTo: true)
+        .snapshots()
+        .listen((snapshot) {
+      _attendees = snapshot.docs.length;
+      notifyListeners();
+    });
     
     FirebaseAuth.instance.userChanges().listen((user) {
       if (user != null) {
@@ -46,6 +74,22 @@ class ApplicationState extends ChangeNotifier {
                 message: document.data()['text'] as String,
               ),
             );
+          }
+          notifyListeners();
+        });
+        _attendingSubscription = FirebaseFirestore.instance
+            .collection('attendees')
+            .doc(user.uid)
+            .snapshots()
+            .listen((snapshot) {
+          if (snapshot.data() != null) {
+            if (snapshot.data()!['attending'] as bool) {
+              _attending = Attending.yes;
+            } else {
+              _attending = Attending.no;
+            }
+          } else {
+            _attending = Attending.unknown;
           }
           notifyListeners();
         });
